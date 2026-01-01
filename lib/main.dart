8b8a0fbd5b3d28c3589d55196a69ea66098f907f
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-01-01 20:27:12 -0300

    Funções Anônimas

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ void main() => runApp(PerguntaApp());
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({super.key});
 
+  void responder() {
+    print('Resposta 1 foi selecionada!');
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<String> perguntas = [
@@ -17,9 +21,20 @@ class PerguntaApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             Text(perguntas.elementAt(0)),
-            ElevatedButton(onPressed: () {}, child: const Text('Resposta 1')),
-            ElevatedButton(onPressed: () {}, child: const Text('Resposta 2')),
-            ElevatedButton(onPressed: () {}, child: const Text('Resposta 3')),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Resposta 2 foi selecionada');
+              },
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Resposta 3 foi selecionada!'),
+              child: const Text('Resposta 3'),
+            ),
           ],
         ),
       ),
