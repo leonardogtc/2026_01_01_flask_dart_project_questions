d36e90193dda39c79549cc57a1cfe52919c43532
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-01-01 14:45:53 -0300

    Utlização do Widget ElevateButton que substitiui o RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,22 @@ class PerguntaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual a sua cor favorita?',
+      'Qual o seu animal favorito?',
+    ];
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Perguntas'),
-          ),
-          body: Text('Olá Flutter!!!'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Perguntas')),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas.elementAt(0)),
+            ElevatedButton(onPressed: () {}, child: const Text('Resposta 1')),
+            ElevatedButton(onPressed: () {}, child: const Text('Resposta 2')),
+            ElevatedButton(onPressed: () {}, child: const Text('Resposta 3')),
+          ],
         ),
+      ),
     );
   }
 }
